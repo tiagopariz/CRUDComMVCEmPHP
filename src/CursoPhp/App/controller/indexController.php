a718029c8201cbe22f3aa7220e438893d30cefb0
tiagopariz <tiagopariz@gmail.com>
2018-06-23 11:56:03 -0300

    Aula 1

---
+++ src/CursoPhp/App/controller/indexController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controller;
+
+/**
+ * Description of indexController
+ *
+ * @author alfamidia
+ */
+class indexController {
+    //put your code here
+    public function index() {
+        echo "Página inicial";
+    }
+    public function lista() {
+        echo "LISTA";
+    }
+    public function conteudo() {
+        echo "CONTEUDO";
+    }
+}
---
+++ src/CursoPhp/index.php
@@ -0,0 +1,41 @@
+<?php
+include 'App//controller//indexController.php';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$route = new Route;
+/**
+ * Description of Route
+ *
+ * @author alfamidia
+ */
+class Route {
+    //put your code here
+    private $routes;
+    
+    public function __construct() {
+        $this->initRoutes();
+        $this->run($this->getURL());
+    }
+    
+    public function initRoutes(){
+        $this->routes['/CursoPhp/'] = array ('controller' => 'indexController', 'action' => 'index');
+        $this->routes['/CursoPhp/lista/'] = array ('controller' => 'indexController', 'action' => 'lista');
+        $this->routes['/CursoPhp/conteudo/'] = array ('controller' => 'indexController', 'action' => 'conteudo');
+    }
+    
+    protected function run($url){
+        if (array_key_exists($url, $this->routes)){
+            $class = "\\App\\controller\\" . $this->routes[$url]['controller'];
+            $controller = new $class;
+            $action = $this->routes[$url]['action'];
+            $controller->$action();
+        }
+    }
+    
+    public function getURL(){
+        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+}
